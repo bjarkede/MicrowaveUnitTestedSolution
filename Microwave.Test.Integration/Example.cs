1aa43fd4b99e67360b987e0aec462c460d36f20e
Bjarke <32188250+bjarkede@users.noreply.github.com>
2020-04-22 18:42:14 +0200

    Add integration project.

---
+++ Microwave.Test.Integration/Example.cs
@@ -0,0 +1,19 @@
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
