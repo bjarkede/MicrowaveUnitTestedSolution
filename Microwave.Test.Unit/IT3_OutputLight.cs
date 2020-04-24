4d07f85019f5edae0f49089accf96ce416d465a3
Bjarke <32188250+bjarkede@users.noreply.github.com>
2020-04-24 21:11:37 +0200

    Add IT3.

---
+++ Microwave.Test.Unit/IT3_OutputLight.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Microwave.Test.Unit
+{
+    [TestFixture]
+    class IT3_OutputLight
+    {
+        private ILight _utt;
+        private IOutput _output;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = Substitute.For<IOutput>();
+            _utt = new Light(_output);
+        }
+
+        [Test]
+        public void TurnOn_DisplayIsAnnouncedCorrectly()
+        {
+            _utt.TurnOn();
+            _output.Received().OutputLine("Light is turned on");
+        }
+
+        [Test]
+        public void TurnOff_DisplayIsAnnouncedCorrectly()
+        {
+            _utt.TurnOff();
+            _output.Received().OutputLine("Light is turned off");
+        }
+
+    }
+}
