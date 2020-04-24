a2b4afcf850d7a6e5cf9120c47be149f688b51f5
Bjarke <32188250+bjarkede@users.noreply.github.com>
2020-04-24 18:46:11 +0200

    Finish IT1

---
+++ Microwave.Test.Unit/IT1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Unit
+{
+    [TestFixture]
+    class IT1_OutputPowerTube
+    {
+        private IOutput _output;
+        private PowerTube _utt;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = Substitute.For<IOutput>();
+            _utt = new PowerTube(_output);
+        }
+
+        [Test]
+        public void TurnOff_PowerTubeIsAnnouncedCorrectly()
+        {
+            _utt.TurnOn(50);
+            _utt.TurnOff();
+
+            _output.Received().OutputLine("PowerTube turned off");
+        }
+
+        [Test]
+        public void TurnOn_PowerTubeIsAnnouncedCorrectly()
+        {
+            _utt.TurnOn(50);
+            _output.Received().OutputLine("PowerTube works with 50");
+        }
+    }
+}
